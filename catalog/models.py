2c33a5855ff43bbafa880e53a8925ede62aab89c
Wizard <merihov2yandex.ru>
2026-03-07 20:42:15 +0300

    start work

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# Product: наименование, описание, изображение, категория, цена за покупку, дата создания, дата последнего изменения.
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+    image = models.ImageField(upload_to='home/photo', blank=True, null=True, verbose_name='изображение')
+    category =
+    price = models.FloatField(verbose_name='цена за покупку')
+    created_at = models.DateTimeField(verbose_name='дата создания')
+    updated_at = models.DateTimeField(verbose_name='дата последнего изменения')
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = 'name'
+
