8d5f18fb91fa0e65fedc3ffc8aac6c1807289af1
Wizard <merihov2yandex.ru>
2026-03-09 11:04:20 +0300

    migrate work

--- catalog/models.py
+++ catalog/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Product: наименование, описание, изображение, категория, цена за покупку, дата создания, дата последнего изменения.
 
 class Product(models.Model):
-    name = models.CharField(max_length=150, verbose_name='наименование')
-    description = models.TextField(verbose_name='описание')
+    name = models.CharField(max_length=150, verbose_name='наименование продукта')
+    description = models.TextField(verbose_name='описание продукта', blank=True, null=True)
     image = models.ImageField(upload_to='home/photo', blank=True, null=True, verbose_name='изображение')
-    category =
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, related_name='products')
     price = models.FloatField(verbose_name='цена за покупку')
     created_at = models.DateTimeField(verbose_name='дата создания')
     updated_at = models.DateTimeField(verbose_name='дата последнего изменения')
@@ -14,5 +14,20 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
-        ordering = 'name'
+        ordering = ('name',)
 
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование категории')
+    description = models.TextField(verbose_name='описание категории', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Категрия'
+        verbose_name_plural = 'Категории'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
