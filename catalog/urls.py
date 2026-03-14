58efef691af8c60b7c167710a097562665dea712
Wizard <merihov2yandex.ru>
2026-03-14 20:18:58 +0300

    images

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from catalog.apps import CatalogConfig
 from catalog.views import home, product_detail
@@ -8,3 +10,5 @@ urlpatterns = [
     path('home/', home, name="home"),
     path('product/<int:pk>/', product_detail, name="product_detail"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
