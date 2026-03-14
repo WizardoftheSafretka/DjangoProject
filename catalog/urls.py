e17199a945d653cb28a47f216e0b1458409b8038
Wizard <merihov2yandex.ru>
2026-03-14 19:55:52 +0300

    product_detail.html

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
-from catalog.views import home
+from catalog.views import home, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('home/', home, name="home"),
+    path('product/<int:pk>/', product_detail, name="product_detail"),
 ]
