b18d0bcfebfcb965edc6fa7d30c64aa655f94124
Wizard <merihov2yandex.ru>
2026-03-19 20:19:54 +0300

    Blog_model

---
+++ blogs/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=150, verbose_name='заголовок')
+    content = models.TextField(verbose_name='содержимое', blank=True, null=True)
+    image = models.ImageField(upload_to='blogs/photo', blank=True, null=True, verbose_name='первью')
+    created_at = models.DateField(auto_now_add=True, verbose_name='дата создания')
+    is_published = models.BooleanField(default=0, verbose_name='Опубликовано')
+    count_views = models.PositiveIntegerField(default=0, verbose_name="Количество просмотров", editable=False)
+
+    class Meta:
+        verbose_name = 'блог'
+        verbose_name_plural = 'блоги'
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
+
+    def increase_views(self):
+        """Метод для увеличения счетчика просмотров"""
+
+        self.count_views += 1
+        self.save(update_fields=['count_views'])
--- config/settings.py
+++ config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'catalog',
     'contacts',
+    'blogs',
 ]
 
 MIDDLEWARE = [
