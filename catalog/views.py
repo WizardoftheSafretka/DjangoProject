6764038d7d3b8897187075fe7a377a38492a0e84
Wizard <merihov2yandex.ru>
2026-03-19 19:41:28 +0300

    start work

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from catalog.models import Product
 
+class HomeListView(ListView):
+    model = Product
 
-def home(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, "home.html", context)
+class HomeDetailView(DetailView):
+    model = Product
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {"product": product}
-    return render(request, "product_detail.html", context)
+# def product_detail(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     context = {"product": product}
+#     return render(request, "product_detail.html", context)
