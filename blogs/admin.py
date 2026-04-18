475891bfeeef5e32fce610d94749c25f4d9e761c
Wizard <merihov2yandex.ru>
2026-04-18 17:12:23 +0300

    admin User

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from blogs.models import Blog
+
+
+@admin.register(Blog)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'is_published', 'count_views')
+    list_filter = ('is_published', 'created_at')
+    search_fields = ('title', 'content')
+    readonly_fields = ('count_views',)
