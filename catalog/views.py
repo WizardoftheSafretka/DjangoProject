8d8e729fb995d5b305e9808113795120a195c8d8
Wizard <merihov2yandex.ru>
2026-04-20 18:41:02 +0300

    start work

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,13 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.cache import cache
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseForbidden
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
 from django.views import View
+from django.views.decorators.cache import cache_page
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 
 from catalog.forms import ProductForm
@@ -36,7 +39,16 @@ class OwnerOrModeratorRequiredMixin(UserPassesTestMixin):
 
 class HomeListView(ListView):
     model = Product
+    context_object_name = 'products'
 
+    def get_queryset(self):
+        queryset = cache.get('products_queryset')
+        if not queryset:
+            queryset = super().get_queryset()
+            cache.set('products_queryset', queryset, 60 * 15)
+        return queryset
+
+@method_decorator(cache_page(60 * 15), name='dispatch')
 class HomeDetailView(LoginRequiredMixin, DetailView):
     model = Product
 
